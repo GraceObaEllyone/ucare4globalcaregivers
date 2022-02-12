f39b07a16299f5ce5c31e9904078efc3a834c4e6
GraceObaEllyone <62464629+GraceObaEllyone@users.noreply.github.com>
2022-02-12 09:46:10 +0000

    Ucare4GlobalCareGivers

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, session, redirect
 from flaskext.mysql import MySQL
 
 
@@ -70,6 +70,38 @@ def signUp():
     finally:
         cursor.close()
         conn.close()
+
+@app.route('/showSignin')
+def showSignin():
+    return render_template('signin.html')
+
+
+@app.route('/validateLogin', methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # connect to mysql
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin', (_username,))
+        data = cursor.fetchall()
+
+        if len(data) > 0:
+            if data[0][3] == _password:
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html', error='Wrong Email address or Password.')
+        else:
+            return render_template('error.html', error='Wrong Email address or Password.')
+
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+    finally:
+        cursor.close()
+        con.close()
 
 if __name__ == 'main':
     app.run()
